5d2910fed0d295cbe798346142a81a03c52fc1a6
ssez224 <85168593+ssez224@users.noreply.github.com>
2021-06-01 15:01:50 +0200

    zeld

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ5MjIwNjM3OTQzNzkxNjI3.YLYAWQ.RJxGtSO5GOs1YLHqFQvAAhJvjtA',
+        prefix: '?',
+        activity: 'dnd',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
